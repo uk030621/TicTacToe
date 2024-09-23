15ec6bb00d80e790bbd1b03642c635e90ea196f0
uk030621 <123797901+uk030621@users.noreply.github.com>
2024-09-23 20:31:04 +0100

    More modifications to layout file to deliver custom home page icon

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import Head from "next/head";
 
+// Define local fonts
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -13,22 +13,35 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+// Define metadata including title, description, and icons
 export const metadata = {
   title: "Tic-Tac-Toe Game",
   description: "Developed by LWJ",
+  icons: {
+    icon: "/icons/icon-512x512.png", // Favicon
+    apple: "/icons/icon-180x180.png", // Apple touch icon for iOS home screen
+  },
+  manifest: "/manifest.json", // Link to your Web App Manifest
+};
+
+export const viewport = {
+  themeColor: "#000000", // Set theme color for browsers and devices here
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icons/icon-180x180.png" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+      <head>
+        {/* Add mobile-web-app-capable tags manually */}
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        {/* You can still manually add theme-color here if needed */}
         <meta name="theme-color" content="#000000" />
-      </Head>
-      <body>{children}</body>
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
+
